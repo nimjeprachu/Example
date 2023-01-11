d0e42c1f91caa4df0e988c691767e3c3c2555c23
nimjeprachu <nimjeprachu1995@gmail.com>
2023-01-11 13:33:46 +0530

    add updated file

--- src/test/java/git_Practical/demo.java
+++ src/test/java/git_Practical/demo.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class demo {
@@ -24,6 +25,9 @@ public class demo {
 		driver.findElement(By.id("continue")).click();
 		driver.findElement(By.id("ap_password")).sendKeys("Prachi");
 		driver.findElement(By.id("signInSubmit")).click();
+		String actual=driver.findElement(By.id("nav-link-accountList-nav-line-1")).getText();
+		String expected="Hello Prachi";
+		Assert.assertEquals(actual, expected);
 		driver.close();
 		
 	}
